a7101b4f80a732fabf800e58862536afc90c19e6
Jörg Wegener <joergw@identage.com>
2024-01-29 19:30:22 +0100

    AutoCompletion for nvim added

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -11,6 +11,7 @@ return {
       require("mason-lspconfig").setup {
         ensure_installed = {
           "cssls",
+          "emmet_ls",
           "eslint",
           "lua_ls",
           "tsserver",
@@ -22,9 +23,15 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
       local lspconfig = require "lspconfig"
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities.textDocument.completion.completionItem.snippetSupport = true
+
       lspconfig.lua_ls.setup {}
       lspconfig.tsserver.setup {}
       lspconfig.cssls.setup {}
+      lspconfig.emmet_ls.setup {
+        capabilities = capabilities,
+      }
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
