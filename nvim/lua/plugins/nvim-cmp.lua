6cddba787b1204d1f541d7b08a2e5bde834a2edd
Jörg Wegener <joergw@identage.com>
2024-04-14 12:17:18 +0200

    Intellisense Unterstützung verbessert

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -1,21 +1,32 @@
 return {
-  {
-    "L3MON4D3/LuaSnip",
-    dependencies = {
-      "saadparwaiz1/cmp_luasnip",
-      "rafamadriz/friendly-snippets",
-    }
+  'hrsh7th/nvim-cmp',
+  event = 'InsertEnter',
+  dependencies = {
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    {
+      'L3MON4D3/LuaSnip',
+      version = 'v2.*',
+      build = 'make install_jsregexp',
+    },
+    'saadparwaiz1/cmp_luasnip',
+    'rafamadriz/friendly-snippets',
+    'onsails/lspkind.nvim',
   },
-  {
-    "hrsh7th/nvim-cmp",
-    config = function()
+  config = function()
       local cmp = require "cmp"
+      local luasnip = require 'luasnip'
+      local lspkind = require 'lspkind'
+
       require("luasnip.loaders.from_vscode").lazy_load()
 
       cmp.setup {
+        completion = {
+          completeopt = 'menu,menuone,preview,noselect',
+        },
         snippet = {
           expand = function(args)
-            require("luasnip").lsp_expand(args.body)
+            luasnip.lsp_expand(args.body)
           end,
         },
         window = {
@@ -23,6 +34,8 @@ return {
           documentation = cmp.config.window.bordered(),
         },
         mapping = cmp.mapping.preset.insert {
+          ['<C-k>'] = cmp.mapping.select_prev_item(),
+          ['<C-j>'] = cmp.mapping.select_next_item(),
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
@@ -31,10 +44,15 @@ return {
         },
         sources = cmp.config.sources({
           { name = "luasnip" },
-        }, {
           { name = "buffer" },
+          { name = 'path' },
         }),
+        formatting = {
+          format = lspkind.cmp_format({
+            maxwidth = 50,
+            ellipsis_char = '...',
+          }),
+        },
       }
     end,
-  },
 }
