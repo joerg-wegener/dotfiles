238b45cd5a24e43a8db22d8c42ac9b8eb2f7abf1
Jörg Wegener <joergw@identage.com>
2024-04-14 09:53:06 +0200

    Treesitter in neovim verbessert

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,36 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
+  'nvim-treesitter/nvim-treesitter',
+  event = { 'BufReadPre', 'BufNewFile' },
+  build = ':TSUpdate',
+  dependencies = {
+    'windwp/nvim-ts-autotag',
+  },
   config = function()
-    local config = require "nvim-treesitter.configs"
+    local config = require 'nvim-treesitter.configs'
     config.setup {
-      ensure_installed = { "c_sharp", "css", "html", "javascript", "json", "lua" },
+      ensure_installed = {
+        'c_sharp',
+        'css',
+        'html',
+        'javascript',
+        'json',
+        'lua',
+      },
       highlight = {
         enable = true,
       },
       indent = {
         enable = true,
+      },
+      autotag = {
+        enabled = true,
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = '<C-space>',
+          node_incremental = '<C-space>',
+        },
       },
     }
   end,
