d0b577e13dbaef58f11afc33e5aef69927f04d2a
Jörg Wegener <joergw@identage.com>
2024-04-14 12:58:49 +0200

    chore: alle lua plugins geprettified

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,26 +1,26 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  event = { 'BufReadPre', 'BufNewFile' },
-  build = ':TSUpdate',
+  "nvim-treesitter/nvim-treesitter",
+  event = { "BufReadPre", "BufNewFile" },
+  build = ":TSUpdate",
   dependencies = {
-    'windwp/nvim-ts-autotag',
+    "windwp/nvim-ts-autotag",
   },
   config = function()
-    local config = require 'nvim-treesitter.configs'
+    local config = require "nvim-treesitter.configs"
     config.setup {
       ensure_installed = {
-        'bash',
-        'c_sharp',
-        'css',
-        'dockerfile',
-        'gitignore',
-        'html',
-        'javascript',
-        'json',
-        'lua',
-        'markdown',
-        'vim',
-        'yaml',
+        "bash",
+        "c_sharp",
+        "css",
+        "dockerfile",
+        "gitignore",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "vim",
+        "yaml",
       },
       highlight = {
         enable = true,
@@ -34,8 +34,8 @@ return {
       incremental_selection = {
         enable = true,
         keymaps = {
-          init_selection = '<C-space>',
-          node_incremental = '<C-space>',
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
         },
       },
     }
