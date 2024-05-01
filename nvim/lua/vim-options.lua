1299d9a999dbef43aec2ecb852144e8050599a0b
Jörg Wegener <joergw@identage.com>
2024-05-01 10:29:06 +0200

    feat: line-join verbessert - cursor bleibt stehen

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -21,6 +21,9 @@ vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- line join, aber cursor bleibt stehen
+vim.keymap.set("n", "J", "mzJ`z")
+
 -- Zeilennummern
 opt.relativenumber = true
 opt.number = true
