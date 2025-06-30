3b072b653a0b33994951c384dcd0f2fb63c347d6
Jörg Wegener <joergw@identage.com>
2025-06-30 10:00:14 +0200

    fix: setup_handlers von mason-lsp wurde entfernt

--- dotfiles/nvim/.config/nvim/lua/plugins/lsp/lspconfig.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -65,38 +65,40 @@ return {
       vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
     end
 
-    mason_lspconfig.setup_handlers {
-      function(server_name)
-        lspconfig[server_name].setup {
-          capabilities = capabilities,
-        }
-      end,
-      ["emmet_ls"] = function()
-        lspconfig["emmet_ls"].setup {
-          capabilities = capabilities,
-          filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
-        }
-      end,
-      ["lua_ls"] = function()
-        lspconfig["lua_ls"].setup {
-          capabilities = capabilities,
-          settings = {
-            Lua = {
-              diagnostics = {
-                globals = { "vim" },
-              },
-              completion = {
-                callSnippet = "Replace",
+    mason_lspconfig.setup {
+      handlers = {
+        function(server_name)
+          lspconfig[server_name].setup {
+            capabilities = capabilities,
+          }
+        end,
+        ["emmet_ls"] = function()
+          lspconfig["emmet_ls"].setup {
+            capabilities = capabilities,
+            filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
+          }
+        end,
+        ["lua_ls"] = function()
+          lspconfig["lua_ls"].setup {
+            capabilities = capabilities,
+            settings = {
+              Lua = {
+                diagnostics = {
+                  globals = { "vim" },
+                },
+                completion = {
+                  callSnippet = "Replace",
+                },
               },
             },
-          },
-        }
-      end,
-      ["csharp_ls"] = function()
-        lspconfig["csharp_ls"].setup {
-          capabilities = capabilities,
-        }
-      end,
+          }
+        end,
+        ["csharp_ls"] = function()
+          lspconfig["csharp_ls"].setup {
+            capabilities = capabilities,
+          }
+        end,
+      },
     }
   end,
 }
