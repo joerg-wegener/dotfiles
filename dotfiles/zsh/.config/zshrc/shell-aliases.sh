2d80912a9ccda579b86986f66afbf63d22040887
Jörg Wegener <joergw@identage.com>
2025-12-11 07:03:45 +0100

    Weitere Aliase für ls der Konfiguration hinzugefügt

--- dotfiles/zsh/.config/zshrc/shell-aliases.sh
+++ dotfiles/zsh/.config/zshrc/shell-aliases.sh
@@ -1,7 +1,10 @@
 #!/bin/env bash
 
-alias l='ls'
-alias ll='ls -la'
+alias l='ls -h'
+alias ll='ls -hla'
+alias llr='ls -hlar'
+alias lr='ls -hlr'
+alias ltr='ls -hltr'
 
 alias cat="/usr/bin/bat --paging=never"
 alias fzf='fzf --preview "bat --style=numbers --color=always {}"'
