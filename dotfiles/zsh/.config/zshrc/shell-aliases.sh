1f5e6f08fe648ce5a813c42fc061c60422589915
Jörg Wegener <joergw@identage.com>
2025-12-12 15:53:20 +0100

    Fehlendes shell alias hinzugefügt

--- dotfiles/zsh/.config/zshrc/shell-aliases.sh
+++ dotfiles/zsh/.config/zshrc/shell-aliases.sh
@@ -1,10 +1,11 @@
 #!/bin/env bash
 
 alias l='ls -h'
+alias lr='ls -hr'
+alias lt='ls -hlt'
+alias ltr='ls -hltr'
 alias ll='ls -hla'
 alias llr='ls -hlar'
-alias lr='ls -hlr'
-alias ltr='ls -hltr'
 
 alias cat="/usr/bin/bat --paging=never"
 alias fzf='fzf --preview "bat --style=numbers --color=always {}"'
